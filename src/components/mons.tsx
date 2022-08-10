381f0078b750ba8fc3a58b4c8e595be6578f56a6
Monish <monishkumar833@gmail.com>
2022-08-10 13:13:26 -0400

    starting to write function for api data

--- src/components/mons.tsx
+++ src/components/mons.tsx
@@ -1,6 +1,9 @@
 import './mons.css';
 
 function Mon() {
+    let name: string = "";
+    let imgSrc: string = ""
+
 
     function getValue(id: string){
        let element = document.getElementById(id) as HTMLInputElement | null;
@@ -11,15 +14,24 @@ function Mon() {
 
   function getData(){
       let mon = getValue("name");
-      fetch("https://pokeapi.co/api/v2/pokemon"+mon)
+      fetch("https://pokeapi.co/api/v2/pokemon/"+mon)
           .then((response) => response.json())
-          .then((data) => console.log(data));
+          .then((data) => {assignValues(data)});
+          
+    function assignValues(data: any){
+        name = data.name;
+    }
+
   }
 
     return (
         <div>
         <input type="text" id="name"/>
         <button onClick={getData}>yo</button>
+        <div className='info'>
+            Name: {name}
+            <img src= {imgSrc} alt="" />
+        </div>
         </div>
     );
 }
