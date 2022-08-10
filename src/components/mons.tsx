4de54b54405c340ea1ad88a85d1635499343dcd8
Monish <monishkumar833@gmail.com>
2022-08-10 12:55:04 -0400

    more functions

--- src/components/mons.tsx
+++ src/components/mons.tsx
@@ -1,12 +1,25 @@
+
 function Mon() {
+
+    function getValue(id: string){
+       let element = document.getElementById(id) as HTMLInputElement | null;
+       if (element != null){
+       return element.value;
+       }
+    }
+
   function getData(){
-      fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+      let mon = getValue("name");
+      fetch("https://pokeapi.co/api/v2/pokemon"+mon)
           .then((response) => response.json())
           .then((data) => console.log(data));
   }
 
     return (
+        <div>
+        <input type="text" id="name"/>
         <button onClick={getData}>yo</button>
+        </div>
     );
 }
 
