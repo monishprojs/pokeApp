e446ce64e68df8c4b594e48d61ca1d45821e62b3
Monish <monishkumar833@gmail.com>
2022-08-11 13:52:24 -0400

    fixed up random feature as well as code strucutre

--- src/components/mons.tsx
+++ src/components/mons.tsx
@@ -35,6 +35,16 @@ function Mon() {
     const [flavor, setFlavor] = useState('');
     const [isShiny, setShiny] = useState('');
 
+    function randomId() {
+        let random = Math.round(Math.random() * 905)
+        return random as unknown as string
+    }
+
+    function randomSearch() {
+        getData(randomId()!);
+        getData1(randomId()!);
+    }
+
     function getValue(id: string) {
         let element = document.getElementById(id) as HTMLInputElement | null;
         if (element != null) {
@@ -48,91 +58,91 @@ function Mon() {
             .then((data) => {
                 assignValues(data)
             })
+    }
+
+    function getData1(mon: string) {
+        fetch("https://pokeapi.co/api/v2/pokemon-species/" + mon + "/")
+            .then((response) => response.json())
+            .then((data) => {
+                assignValues1(data)
+            })
+    }
 
 
-        function assignValues(data: any) {
-            setName(data.species.name)
-            if (Math.round(Math.random() * 4) === 1) {
-                setImgLink(data.sprites.front_default)
-                setShiny("")
+    function assignValues(data: any) {
+        setName(data.species.name)
+        if (Math.round(Math.random() * 10) !== 1) {
+            setImgLink(data.sprites.front_default)
+            setShiny("")
+        }
+        else {
+            setImgLink(data.sprites.front_shiny)
+            setShiny("You found a shiny!")
+        }
+
+        setType0(data.types[0].type.name)
+        let body = document.querySelector("body");
+        let info = document.getElementById("info")
+        let type0 = document.getElementById("type0")
+        let type1 = document.getElementById("type1")
+        if (body != null && type0 != null) {
+            body.style.backgroundColor = colorsMap.get(data.types[0].type.name)!;
+            type0.style.display = "inline-block";
+            type0.style.backgroundColor = colorsMap.get(data.types[0].type.name)!;
+        }
+        if (data.types.length === 2) {
+            setType1(data.types[1].type.name)
+            if (info != null && type1 != null) {
+                info.style.backgroundColor = colorsMap.get(data.types[1].type.name)!;
+                type1.style.display = "inline-block";
+                type1.style.backgroundColor = colorsMap.get(data.types[1].type.name)!;
+            }
+        }
+        else {
+            setType1('')
+            if (info != null && type1 != null) {
+                info.style.backgroundColor = colorsMap.get(data.types[0].type.name)!;
+                type1.style.display = "none";
+            }
+        }
+        let length = data.moves.length;
+        let array = [0]
+        let i = 0
+        while (i < 4) {
+            let placeholder = Math.round(Math.random() * length);
+            if (!array.includes(placeholder)) {
+                array[i] = placeholder;
+                i++;
+            }
+        }
+        let moves0 = data.moves[array[0]].move.name
+        setMove0(moves0);
+
+        let moves1 = data.moves[array[1]].move.name
+        setMove1(moves1);
+
+        let moves2 = data.moves[array[2]].move.name
+        setMove2(moves2);
+
+        let moves3 = data.moves[array[3]].move.name
+        setMove3(moves3);
+        getData1(getValue("name")!);
+    }
+
+
+    function assignValues1(data: any) {
+        let isEn = false;
+        let i: number = 0;
+        while (isEn === false) {
+            if (data.flavor_text_entries[i].language.name === "en") {
+                setFlavor(data.flavor_text_entries[i].flavor_text)
+                isEn = true;
             }
             else {
-                setImgLink(data.sprites.front_shiny)
-                setShiny("You found a shiny!")
-            }
-
-            setType0(data.types[0].type.name)
-            let body = document.querySelector("body");
-            let info = document.getElementById("info")
-            let type0 = document.getElementById("type0")
-            let type1 = document.getElementById("type1")
-            if (body != null && type0 != null) {
-                body.style.backgroundColor = colorsMap.get(data.types[0].type.name)!;
-                type0.style.display = "inline-block";
-                type0.style.backgroundColor = colorsMap.get(data.types[0].type.name)!;
-            }
-            if (data.types.length === 2) {
-                setType1(data.types[1].type.name)
-                if (info != null && type1 != null) {
-                    info.style.backgroundColor = colorsMap.get(data.types[1].type.name)!;
-                    type1.style.display = "inline-block";
-                    type1.style.backgroundColor = colorsMap.get(data.types[1].type.name)!;
-                }
-            }
-            else {
-                setType1('')
-                if (info != null && type1 != null) {
-                    info.style.backgroundColor = colorsMap.get(data.types[0].type.name)!;
-                    type1.style.display = "none";
-                }
-            }
-            let length = data.moves.length;
-            let array = [0]
-            let i = 0
-            while (i < 4) {
-                let placeholder = Math.round(Math.random() * length);
-                if (!array.includes(placeholder)) {
-                    array[i] = placeholder;
-                    i++;
-                }
-            }
-            let moves0 = data.moves[array[0]].move.name
-            setMove0(moves0);
-
-            let moves1 = data.moves[array[1]].move.name
-            setMove1(moves1);
-
-            let moves2 = data.moves[array[2]].move.name
-            setMove2(moves2);
-
-            let moves3 = data.moves[array[3]].move.name
-            setMove3(moves3);
-            getData1(getValue("name")!);
-        }
-
-        function getData1(mon: string) {
-            fetch("https://pokeapi.co/api/v2/pokemon-species/" + mon + "/")
-                .then((response) => response.json())
-                .then((data) => {
-                    assignValues1(data)
-                })
-        }
-
-        function assignValues1(data: any) {
-            let isEn = false;
-            let i: number = 0;
-            while (isEn === false) {
-                if (data.flavor_text_entries[i].language.name === "en") {
-                    setFlavor(data.flavor_text_entries[i].flavor_text)
-                    isEn = true;
-                }
-                else {
-                    isEn = false
-                    i++
-                }
+                isEn = false
+                i++
             }
         }
-
     }
 
     return (
@@ -141,6 +151,7 @@ function Mon() {
                 <div>
                     <input type="text" id="name" />
                     <button onClick={() => getData(getValue("name")!)}>Search</button>
+                    <button onClick={() => randomSearch()}>Random</button>
                 </div>
                 <div>
                     {isShiny}
@@ -180,6 +191,9 @@ function Mon() {
             </div>
         </div>
     );
+
 }
+
+
 
 export default Mon;
